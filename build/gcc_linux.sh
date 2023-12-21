43049ba89d8d7e911a53941acf3d6728722e5970
Dmitry <dmitritr@gmail.comgit>
2023-12-21 23:34:42 +0300

    gcc build script fixed (-static added for cross-linux compatibility of binary executable)

--- build/gcc_linux.sh
+++ build/gcc_linux.sh
@@ -2,4 +2,5 @@ echo $'\n'Building Thicket for native Linux platform using gcc... $'\n'
 ODIR="output/$(uname -s)_$(uname -i)"
 mkdir -p "${ODIR}";
 
-gcc -std=c++17 "-o${ODIR}/thicket" ../src/cornus_thicket/main.cpp  -lstdc++
+gcc -static -std=c++17 "-o${ODIR}/thicket" ../src/cornus_thicket/main.cpp  -lstdc++
+
