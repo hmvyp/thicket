e4a1b9ae12387dc6f0bf9b2344caf25359753083
Dmitry <dmitritr@gmail.comgit>
2024-09-25 15:04:53 +0300

    imprint ...  On branch dev2 	modified:   src/cornus_thicket/imprint.hpp

--- src/cornus_thicket/imprint.hpp
+++ src/cornus_thicket/imprint.hpp
@@ -1,6 +1,7 @@
 #ifndef cornus_thicket_imprint_hpp
 #define cornus_thicket_imprint_hpp
 
+#include <string_view>
 #include  "node.hpp"
 
 
@@ -11,8 +12,8 @@ class Imprint {
     // Artifact type:
     enum NodeStatus {
         nsNONE,
-        nsEXISING, // not a thicket artifact (pre-existed here)
-        nsLINK, // symbolic link
+        nsEXISING, // not a thicket artifact (pre-existed)
+        nsLINK, // symbolic link (an artifact)
         nsCOPY //  an artifact, but not a link, i.e. actual file/directory created by thicket
     };
 
@@ -23,7 +24,13 @@ class Imprint {
 
 private:
     std::string // error
-    parseRecord(const std::string& line){
+    decodeRecord(const std::string& line){
+
+        // expected record structure example: cd:relative/path/from/scope
+        // 'c' - "copy"
+        // 'd' - "directory"
+        // ':' - delimiter
+
         size_t len = line.length();
         if(len == 0 || line[0] == '#'){
             return ""; // skip empty or comments
@@ -65,13 +72,43 @@ private:
             return "Imprint record syntax: 3d symbol shall be ':' ";
         }
 
-        all_nodes_[line.substr(3)] = rc;
+        all_records_[line.substr(3)] = rc;
 
         return "";
     }
 
 
-    std::map<std::string, Record> all_nodes_;
+    std::string // error or ""
+    addRecord(const std::string& scope_path, Node& n, NodeStatus ns){
+        if(n.node_type == UNKNOWN_NODE_TYPE){
+            return "Internal Thicket error: "" Can not encode imprint record: node type (file or directory) is unknown";
+        }
+
+        if(n.ref_type == FS_NODE && ns > nsEXISING) {
+            return "Internal Thicket error:  Can not encode imprint record: pre-existed node can not be a link or copy";
+        }
+
+        if(n.ref_type == REFERENCE_NODE && ns == nsEXISING) {
+            return "Internal Thicket error:  Can not encode imprint record: virtual (reference) node can not have nsEXISING status";
+        }
+
+        const std::string_view npsv = std::string_view(n.path_as_string_);
+        const std::string_view sp_sv = std::string_view(scope_path);
+
+        // check if the node path is really prefixed with the scope path (paranoia):
+        if(npsv.length() < sp_sv.length() || npsv.substr(0, sp_sv.length()) != sp_sv) {
+            return "Internal Thicket error:  for a node under the scope the node path is not prefixed with the scope path ";
+        }
+
+        Record rc = {n.node_type, ns};
+
+        all_records_[std::string(npsv.substr(sp_sv.length()))] = rc;
+
+        return "";
+    }
+
+
+    std::map<std::string, Record> all_records_;
 
 };
 
