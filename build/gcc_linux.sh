cb44e8053ffb309f3d20816ef3039cef9d7ed1d5
Dmitry <dmitritr@gmail.comgit>
2025-06-13 23:12:33 +0300

    build script: -O3 optimization added 	modified:   gcc_linux.sh

--- build/gcc_linux.sh
+++ build/gcc_linux.sh
@@ -2,5 +2,5 @@ echo $'\n'Building Thicket for native Linux platform using gcc... $'\n'
 ODIR="output/$(uname -s)_$(uname -i)"
 mkdir -p "${ODIR}";
 
-gcc -static -std=c++17 -I../src "-o${ODIR}/thicket" ../src/cornus_thicket/main.cpp  -lstdc++
+gcc -static -std=c++17 -O3 -I../src "-o${ODIR}/thicket" ../src/cornus_thicket/main.cpp  -lstdc++
 
